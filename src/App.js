40c5bbdae0fbda039ab0b39ebc133813920d7224
shahroze Gondal <gondalshahroze@gmail.com>
2022-11-17 15:24:54 +0500

    useState Assignemt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [inputValue, setInput]=useState('');
+  // const [show, setShow]=useState(false);
+
+  // const toggle=()=>{
+  //   const doesShow = show;
+  //   setShow(!show);
+  // }
+
+  // const changeInputValue=(event)=>{
+  //   setInput(event.target.value);
+
+  const [count, setCount] = useState(0);
+  const increaseHandler = ()=>{
+    setCount(count+1);
+  }
+  const decreaseHandler=()=>{
+    setCount(count-1);
+  }
+  const setToZero=()=>{
+    setCount(0);
+  }
+
   return (
     <div className="App">
-      <Job name='dsf'/>
+      {/* <input type='text' onChange={changeInputValue}></input>
+      {inputValue}
+      <button onClick={toggle}>Toggle</button>
+      {
+        show && <h1>this is the show</h1>
+      } */}
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>decrease</button>
+      <button onClick={setToZero}>set to Zero</button>
+      <h1>{count}</h1>
     </div>
   );
-}
-
-
-const Job = (props) => {
-  return (
-    <div>
-      <p>{props.name}</p>
-      <p></p>
-      <p></p>
-    </div>
-  )
 }
 
 
