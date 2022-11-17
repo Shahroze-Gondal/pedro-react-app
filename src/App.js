4432f45aae2a14c40e7d8f559da6a442b0bb2fb5
shahroze Gondal <gondalshahroze@gmail.com>
2022-11-17 18:56:18 +0500

    api_call

--- src/App.js
+++ src/App.js
@@ -3,20 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Job name='dsf'/>
     </div>
   );
-}
-
-
-const Job = (props) => {
-  return (
-    <div>
-      <p>{props.name}</p>
-      <p></p>
-      <p></p>
-    </div>
-  )
 }
 
 
