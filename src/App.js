bc07253c7efe2560099474e8352d7e0439c842dc
shahroze Gondal <gondalshahroze@gmail.com>
2022-11-17 19:31:23 +0500

    -a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [catFact, setCatFact] = useState('')
+  useEffect(()=>{
+    fetch("https://catfact.ninja/fact")
+    .then((res) => res.json())
+    .then((data)=>{
+      setCatFact(data.fact)
+    })
+  },[])
+
   return (
     <div className="App">
+      <div><button>Cat fact</button></div>
+      <h1>{catFact}</h1>
     </div>
   );
 }
